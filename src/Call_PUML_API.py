5cb8b9f5e325bf777afb7682ed8eb3890d47f045
RezaGolpayegani <rzgolpayegani@yahoo.com>
2024-03-01 09:59:09 -0600

    Reimplmented Call PUML API Completely.

--- src/Call_PUML_API.py
+++ src/Call_PUML_API.py
@@ -1,17 +1,51 @@
 import os
 import shutil
 
+# Specify the directory path
+directory_path = "SD"
+
+# Get a list of all items (directories and files) in the directory
+all_items = os.listdir(directory_path)
+
+# Filter out directories from the list of items
+directories = [item for item in all_items if os.path.isdir(os.path.join(directory_path, item))]
+
+# Sort the list of directories based on creation time (latest first)
+directories.sort(key=lambda x: os.path.getctime(os.path.join(directory_path, x)), reverse=True)
+
+# Get the latest created folder
+latest_folder = directories[0] if directories else None
+
+print("Latest created folder:", latest_folder)
+
 # Source and destination folders
-source_folder = "SD/SDt"
-destination_folder = "SD/SDi"
+source_folder = f"SD/{latest_folder}/SDt"
+destination_folder = f"SD/{latest_folder}/SDi"
 text_files_list = [file for file in os.listdir(source_folder) if file.endswith(".txt")]
 
-os.makedirs(destination_folder, exist_ok=True)
+# Iterate through all files in the directory
+for filename in os.listdir(source_folder):
+    # Check if the file is a text file (ends with .txt)
+    if filename.endswith(".txt"):
+        # Check if the filename contains a space
+        if " " in filename:
+            # Replace the space with underscore
+            new_filename = filename.replace(" ", "_")
+            # Construct the old and new file paths
+            old_filepath = os.path.join(source_folder, filename)
+            new_filepath = os.path.join(source_folder, new_filename)
+            # Rename the file
+            os.rename(old_filepath, new_filepath)
+            print(f"Renamed {filename} to {new_filename}")
 
+
+os.makedirs(destination_folder, exist_ok=True)
+# print(os.listdir(source_folder))
 # Generate diagrams using the PlantUML command
 for SDt in os.listdir(source_folder):
-    file_name = f"SD/SDt/{SDt}"
+    file_name = f"SD/{latest_folder}/SDt/{SDt}"
     os.system(f"python -m plantuml {file_name}")
+    # print(file_name)
 
 # Create the destination folder if it doesn't exist
 if not os.path.exists(destination_folder):
@@ -30,5 +64,3 @@ for file in files:
         
         # Move the file to the destination folder
         shutil.move(source_path, destination_path)
-
-print("Image generation was successful.")
