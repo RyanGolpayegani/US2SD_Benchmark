520fd9d26810cf8befdab0f84b7e4d69e0c70fcb
RezaGolpayegani <rzgolpayegani@yahoo.com>
2024-03-25 10:51:20 -0600

    us+BERTSUM SDi and missing

--- src/find_missing.py
+++ src/find_missing.py
@@ -1,9 +1,9 @@
 import os
 
 # Specify the paths to your text and picture folders
-text_folder = "Experiment/US BART/SDt"
-picture_folder = "Experiment/US/SDi"
-main_folder = "Experiment/US BART"
+text_folder = "Experiment/US BERTSUM/SDt"
+picture_folder = "Experiment/US BERTSUM/SDi"
+main_folder = "Experiment/US BERTSUM"
 
 # Extract base filenames (without extensions) from the text files
 text_filenames = [os.path.splitext(f)[0] for f in os.listdir(text_folder) if f.endswith(".txt")]
