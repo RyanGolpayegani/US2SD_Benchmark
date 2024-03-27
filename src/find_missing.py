36c93819516491ba3f352051652f0a3eef9010d0
RezaGolpayegani <rzgolpayegani@yahoo.com>
2024-03-27 15:48:22 -0600

    GPT4 Turbo SDi

--- src/find_missing.py
+++ src/find_missing.py
@@ -1,9 +1,9 @@
 import os
 
 # Specify the paths to your text and picture folders
-text_folder = "Experiment/US GPT4/SDt"
-picture_folder = "Experiment/US GPT4/SDi"
-main_folder = "Experiment/US GPT4"
+text_folder = "Experiment/US GPT4 Turbo/SDt"
+picture_folder = "Experiment/US GPT4 Turbo/SDi"
+main_folder = "Experiment/US GPT4 Turbo"
 
 # Extract base filenames (without extensions) from the text files
 text_filenames = [os.path.splitext(f)[0] for f in os.listdir(text_folder) if f.endswith(".txt")]
