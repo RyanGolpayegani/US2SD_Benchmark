cc8ad8da40505590e49af9432ce23ac048588e2c
RezaGolpayegani <rzgolpayegani@yahoo.com>
2024-04-24 10:09:18 -0600

    plots completed

--- src/Eval_vis.py
+++ src/Eval_vis.py
@@ -37,17 +37,134 @@ fig = plt.figure(figsize=(9, 11))
 
 plot = sns.catplot(
     data=df,
-    x="folder name",
-    y="messages",
+    y="folder name",
+    x="messages",
     # hue='cohesion',
     kind='box',
     palette = 'pastel'
 )
 # palettes: ch:.25, Blues,
 # plt.title("Number of Messages generated with each approach")
-plt.ylabel("Messages")
+plt.ylabel("Approach")
+plt.xlabel("Messages")
+plt.xticks(rotation=90)
+
+# plt.savefig("../images/1.png", dpi=600, bbox_inches = 'tight')
+
+
+
+
+
+
+#%%
+df.head()
+
+# %%
+fig = plt.figure(figsize=(9, 11))
+
+plot = sns.catplot(
+    data=df,
+    x="folder name",
+    y="lifelines",
+    # hue='cohesion',
+    kind='bar',
+    palette = 'pastel'
+)
+# palettes: ch:.25, Blues,
+# plt.title("Number of Messages generated with each approach")
+plt.ylabel("Lifelines")
 plt.xlabel("Approach")
 plt.xticks(rotation=90)
 
-plt.savefig("../images/2.png", dpi=600, bbox_inches = 'tight')
+plt.savefig("../images/3.png", dpi=600, bbox_inches = 'tight')
+# %%
+
+fig = plt.figure(figsize=(9, 11))
+
+plot = sns.catplot(
+    data=df,
+    x="folder name",
+    y="nesting_depth",
+    # hue='cohesion',
+    kind='bar',
+    palette = 'pastel',
+    ci=None,
+)
+# palettes: ch:.25, Blues,
+# plt.title("Number of Messages generated with each approach")
+plt.ylabel("Nesting Depth")
+plt.xlabel("Approach")
+plt.xticks(rotation=90)
+
+plt.savefig("../images/4.png", dpi=600, bbox_inches = 'tight')
+# %%
+
+fig = plt.figure(figsize=(9, 11))
+
+plot = sns.catplot(
+    data=df,
+    x="folder name",
+    y="cyclomatic_complexity",
+    # hue='cohesion',
+    kind='bar',
+    palette = 'pastel',
+    ci=None,
+)
+# palettes: ch:.25, Blues,
+# plt.title("Number of Messages generated with each approach")
+plt.ylabel("Cyclomatic Complexity")
+plt.xlabel("Approach")
+plt.xticks(rotation=90)
+
+plt.savefig("../images/5.png", dpi=600, bbox_inches = 'tight')
+# %%
+df['cohesion_numeric'] = df['cohesion'].apply(lambda x: 0 if x == 'Medium/Low' else 1 if x == 'High' else None)
+df.head()
+#%%
+fig = plt.figure(figsize=(9, 11))
+
+plot = sns.catplot(
+    data=df,
+    x="folder name",
+    y="cohesion_numeric",
+    # hue='cohesion',
+    kind='bar',
+    palette = 'pastel',
+    ci=None,
+)
+# palettes: ch:.25, Blues,
+# plt.title("Number of Messages generated with each approach")
+plt.ylabel("Cohesion")
+plt.xlabel("Approach")
+plt.xticks(rotation=90)
+
+plt.savefig("../images/6.png", dpi=600, bbox_inches = 'tight')
+
+
+#%%
+# cohesion,coupling,
+Counter(df["coupling"])
+# %%
+df['coupling_numeric'] = df['coupling'].apply(lambda x: 0 if x == 'Medium/Low' else 1 if x == 'High' else None)
+df.head()
+fig = plt.figure(figsize=(9, 11))
+
+plot = sns.catplot(
+    data=df,
+    x="folder name",
+    y="coupling_numeric",
+    # hue='cohesion',
+    kind='bar',
+    palette = 'pastel',
+    ci=None,
+)
+# palettes: ch:.25, Blues,
+# plt.title("Number of Messages generated with each approach")
+plt.ylabel("Coupling")
+plt.xlabel("Approach")
+plt.xticks(rotation=90)
+
+plt.savefig("../images/7.png", dpi=600, bbox_inches = 'tight')
+
+
 # %%
